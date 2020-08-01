4d403e41d0469c8d57dd19d2f25154449528079e
JosephineAkello <Josephineakello2030@gmail.com>
2020-08-02 01:14:48 +0300

    add path variable

--- src/main/java/com/example/springmvcdemo/controller/TodoRestController.java
+++ src/main/java/com/example/springmvcdemo/controller/TodoRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.example.springmvcdemo.model.Todo;
 import com.example.springmvcdemo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,10 @@ public class TodoRestController {
     public List<Todo> listAllTodos() {
         List<Todo> users = service.retrieveTodos("in28Minutes");
         return users;
+    }
+
+    @RequestMapping(value = "/todo/{id}", method = RequestMethod.GET)
+    public Todo retrieveTodo(@PathVariable("id") int id) {
+        return service.retrieveTodo(id);
     }
 }
