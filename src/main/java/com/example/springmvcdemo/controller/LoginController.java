9a05dc64c7361a1de4903b5145f28d8d2d278d2f
JosephineAkello <Josephineakello2030@gmail.com>
2020-08-01 14:10:39 +0300

    add post request method

--- src/main/java/com/example/springmvcdemo/controller/LoginController.java
+++ src/main/java/com/example/springmvcdemo/controller/LoginController.java
@@ -1,15 +1,23 @@
 package com.example.springmvcdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
-    @ResponseBody
-    public String sayHello(){
-        return "hello world";
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLoginPage(){
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String handleLoginRequest(@RequestParam String name, ModelMap model){
+       model.put("name", name);
+        return "welcome";
     }
 }
